c6606a6f0137ca8d7be4f2e16a549b0f9b5a3d00
Louis Lu <lhy2002m@gmail.com>
2024-09-29 20:25:55 -0400

    Initial commit

---
+++ src/main/java/com/ScalableDynamics/Jobs_Microservice/JobsMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.ScalableDynamics.Jobs_Microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobsMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobsMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ScalableDynamics/Jobs_Microservice/JobsMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ScalableDynamics.Jobs_Microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobsMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
