c5742e4f990d1d56bc25dd623aeb2c5f3f07b596
Louis <87416327+LouisLu00@users.noreply.github.com>
2024-10-29 23:26:30 -0400

    Added CORS exemption (#10)
    * Added CORS exemption for development/debug
    
    * Added CORS exemption for development/debug

--- src/main/java/com/ScalableDynamics/Jobs_Microservice/Config/CorsConfig.java
+++ src/main/java/com/ScalableDynamics/Jobs_Microservice/Config/CorsConfig.java
@@ -7,6 +7,12 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+
 @Configuration
 @EnableWebSecurity
 public class CorsConfig {
@@ -20,12 +26,23 @@ public class CorsConfig {
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
             .csrf(csrf -> csrf.disable())  // Disables CSRF globally
-            // Permit all requests temporarily for debugging
+            .cors(withDefaults())  // Enables CORS with default settings
             .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll());
 
     return http.build();
   }
 
+  @Bean
+  public CorsFilter corsFilter() {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowCredentials(true);  // Allows credentials in CORS requests
+    config.addAllowedOriginPattern("*");  // Adjust to specific origins if needed
+    config.addAllowedHeader("*");
+    config.addAllowedMethod("*");
+    source.registerCorsConfiguration("/**", config);
+    return new CorsFilter(source);
+  }
 
 
 }
